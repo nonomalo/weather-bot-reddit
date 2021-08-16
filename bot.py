59673bc3be073f7fdadf435a1eaee796a6b3f0b6
nonomalo <noah.lopez3@gmail.com>
2021-08-16 16:17:17 -0700

    Program is now connected to reddit

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
-print("test")
+import praw
+import config
+
+reddit = praw.Reddit(username = config.username,
+                    password = config.password,
+                    client_id = config.client_id,
+                    user_agent = "current-weather-bot v0.0.1")
+
+reddit.validate_on_submit=True
+
+subreddit = reddit.subreddit("test")
+
+subreddit.submit("Test Submission", url="https://reddit.com")
