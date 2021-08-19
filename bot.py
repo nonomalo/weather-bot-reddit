fbb452570e7f6429bfc08c3b417c0f32ac5ce76e
nonomalo <noah.lopez3@gmail.com>
2021-08-19 11:23:47 -0700

    Receives json data and parses into python

--- bot.py
+++ bot.py
@@ -1,26 +1,33 @@
 import praw
 import config
 import requests
+import json
 
-reddit = praw.Reddit(username = config.username,
-                    password = config.password,
-                    client_id = config.client_id,
-                    weather_api = config.weather_api,
-                    user_agent = "current-weather-bot v0.0.1")
+#reddit = praw.Reddit(username = config.username,
+#                    password = config.password,
+#                    client_id = config.client_id,
+#                    weather_api = config.weather_api,
+#                    user_agent = "current-weather-bot v0.0.1")
 
-reddit.validate_on_submit=True
+#reddit.validate_on_submit=True
 
-subreddit = reddit.subreddit("test")
+#subreddit = reddit.subreddit("test")
 
 # make a post on "test" subreddit
-subreddit.submit("Test Submission", url="https://reddit.com")
+#subreddit.submit("Test Submission", url="https://reddit.com")
 
 # get full weather url
 weather_url = "http://api.openweathermap.org/data/2.5/weather?"
 full_url = weather_url + f"q=Seattle&appid={config.weather_api}"
 
 # have bot respond to comments with its name
-for comment in subreddit.stream.comments():
-    if comment.body.startswith("!current-weather-bot"):
-        weather_request = requests.get(full_url)
-        comment.reply(weather_request)
+#for comment in subreddit.stream.comments():
+#    if comment.body.startswith("!current-weather-bot"):
+#        weather_request = requests.get(full_url)
+#        response = f"City: {weather_request.name}"
+#        comment.reply(weather_request)
+
+weather_request = requests.get(full_url)
+weather_data = weather_request.json()
+response = f"City: {weather_data['name']}"
+print(response)
